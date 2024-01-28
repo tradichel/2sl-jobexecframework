0389276f53d3821c4836a0bda92d7de4851ed0aa
Teri Radichel <nunya>
2024-01-28 00:44:59 +0000

    take local out of run till I figure out what's wrong

--- aws/job/run.sh
+++ aws/job/run.sh
@@ -10,18 +10,17 @@ source shared/validate.sh
 source shared/functions.sh
 source resources/ssm/parameter/parameter_functions.sh
 
-main(){
 
   #configure job role CLI profile
-	local parameters="$1"
-	local profile=$(get_container_parameter_value $parameters "profile")
-	local access_key=$(get_container_parameter_value $parameters "accesskey")
-	local secret_key=$(get_container_parameter_value $parameters "secretaccesskey")
-	local session_token=$(get_container_parameter_value $parameters "sessiontoken")
-  local region=$(get_container_parameter_value $parameters "region")
-	local job_config_ssm_parameter=$(get_container_parameter_value $parameters "jobconfig")
+	parameters="$1"
+	profile=$(get_container_parameter_value $parameters "profile")
+	access_key=$(get_container_parameter_value $parameters "accesskey")
+	secret_key=$(get_container_parameter_value $parameters "secretaccesskey")
+	session_token=$(get_container_parameter_value $parameters "sessiontoken")
+  region=$(get_container_parameter_value $parameters "region")
+	job_config_ssm_parameter=$(get_container_parameter_value $parameters "jobconfig")
   	
-	local s="job/run.sh"
+	s="job/run.sh"
 	validate_set $s "profile" $profile
 	validate_set $s "access_key" $access_key
 	validate_set $s "secret_key" $secret_key
@@ -51,7 +50,5 @@ main(){
 	echo "### execute the job - the execution script has container specific execution code ###"
 	./execute.sh $profile $job_config_ssm_parameter
 	
-}
 
-main
 
